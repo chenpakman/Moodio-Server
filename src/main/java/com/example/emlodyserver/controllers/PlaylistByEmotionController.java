d2ed5376598aba9c39d618a32edd281f21412058
Taly96 <talicht96@gmail.com>
2023-04-15 20:02:52 +0300

    multiple_playlists

--- src/main/java/com/example/emlodyserver/controllers/PlaylistByEmotionController.java
+++ src/main/java/com/example/emlodyserver/controllers/PlaylistByEmotionController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.google.gson.Gson;
 
@@ -24,21 +21,24 @@ public class PlaylistByEmotionController {
     @Value("${project.image}")
     private String path;
     private SpotifyApiManager spotifyApiManager=new SpotifyApiManager();
-    ResponseServer response = new ResponseServer();
+    ResponseServer response ;
     String emotion;
 
     @PostMapping(value = "/app")
     public ResponseEntity<String> fileUpload(@RequestParam("image") MultipartFile image) {
         System.out.println("Here");//TODO:delete
         Gson gson = new Gson();
-        Boolean isRelaxing = false;
-        Boolean isHappy = false;
+        boolean isRelaxing = false;
+        boolean isHappy = false;
+        this.response = new ResponseServer();
 
         try {
+
             String resEmotion = this.fileService.getEmotionByImage(path, image);
 
             if (null != resEmotion && !resEmotion.isEmpty()) {
                 System.out.println(resEmotion); //TODO:delete
+
                 this.emotion =
                         resEmotion.replace(resEmotion.charAt(0), resEmotion.substring(0,1).toUpperCase().charAt(0));
                 this.response.setEmotion(this.emotion);
@@ -62,7 +62,8 @@ public class PlaylistByEmotionController {
                         isHappy = true;
                         break;
                     default:
-                        break;// TODO: ask what the user wants to listen to? show featured playlist?
+                        response.setError(Errors.getInvalidImage());
+                        return new ResponseEntity<>(gson.toJson(response), HttpStatus.NO_CONTENT);
                 }
 
                 if(isRelaxing){
@@ -71,7 +72,7 @@ public class PlaylistByEmotionController {
                     this.response.addPlaylistUrl("Relaxing", relaxingUrl);
                 }
                 if(isHappy){
-                    this.response.setPlaylistUrl(this.spotifyApiManager.getPlaylistUrl("Good Mood Happy"));
+                    this.response.setPlaylistUrl(this.spotifyApiManager.getPlaylistUrl("Happy"));
                     String happyUrl = this.spotifyApiManager.getPlaylistUrl("Happy");
                     this.response.addPlaylistUrl("Happy", happyUrl);
                 }
@@ -79,19 +80,33 @@ public class PlaylistByEmotionController {
                 return new ResponseEntity<>(gson.toJson(response), HttpStatus.OK);
             } else {
                 response.setError(Errors.getInvalidImage());
-                return new ResponseEntity<>(gson.toJson(response), HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(gson.toJson(response), HttpStatus.NO_CONTENT);
             }
         } catch (IOException e) {
             response.setError(e.getMessage());
-            return new ResponseEntity<>(gson.toJson(response), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(gson.toJson(response), HttpStatus.NO_CONTENT);
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
     }
+
+    @PutMapping(value = "/app")
+    public ResponseEntity<String> getPlayListsWithoutDeepFace(@RequestParam ("emotions") String emotions) throws IOException {
+        Gson gson = new Gson();
+        String playlistUrl = this.spotifyApiManager.getPlaylistUrl(emotions);
+        this.response.addPlaylistUrl(emotion, playlistUrl);
+        return new ResponseEntity<>(gson.toJson(response), HttpStatus.OK);
+    }
+
     @GetMapping(value = "/app")
     public void fileUpload() throws IOException {
 
     }
-    }
+
+
+
+
+}
+
